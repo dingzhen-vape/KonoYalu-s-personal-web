7fbb0b941b932094e8da47f40d07f4f8b7e85d3f
dingzhen-vape <327112257@qq.com>
2026-08-02 20:28:52 +0800

    feat(app): 添加 Bing 每日壁纸背景与导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
-<script setup lang="ts">
+
+
+
+<script lang="ts">
 import HomePage from './components/HomePage.vue'
+import axios from 'axios';
+
+export default {
+  components: {
+    HomePage
+  },
+  data() {
+    return {
+      DailyWallpaper: ''
+    }
+  },
+  async created() {
+    this.DailyWallpaper = await getDailyWallpaper()
+  }
+}
+
+async function getDailyWallpaper() {
+  try {
+    const res = await axios.get('https://bing.biturl.top/?format=json&resolution=UHD')
+    return res.data.url
+  } catch (error) {
+    console.error('Error parsing JSON:', error)
+    return null
+  }
+}
 </script>
 
 <template>
+  <img :src="DailyWallpaper" />
   <main>
     <HomePage></HomePage>
   </main>
 </template>
-
-<style scoped></style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="Background">
-    <div id="nav bar">
-      HELLO WORLD
+    <div id="NavBar">
+      <ul style="
+       display: flex;
+       justify-content: space-around;
+       list-style: none;">
+        <li>主页</li>
+        <li>关于</li>
+        <li>联系</li>
+      </ul>
     </div>
   </div>
 </template>
