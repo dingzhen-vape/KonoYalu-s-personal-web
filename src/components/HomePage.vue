e2bccb3486dfc7d177e949e85dadecee7ac49215
dingzhen-vape <327112257@qq.com>
2026-08-03 23:53:20 +0800

    feat(ui): 添加 GSAP 依赖并实现主页布局结构

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,14 +1,27 @@
 <template>
+  <!-- 背景 -->
+  
   <div id="Background">
+    <!-- 导航栏 -->
     <div id="NavBar">
       <ul style="
        display: flex;
        justify-content: space-around;
-       list-style: none;">
+       list-style: none;
+       padding: 0;">
         <li>主页</li>
         <li>关于</li>
         <li>联系</li>
       </ul>
+    </div>
+    <!-- 导航栏end -->
+     
+    <!-- 信息栏 -->
+
+    <div id="Info">
+      <div id="InfoLeft">
+        <h1>食我压路's主页</h1> 
+      </div>
     </div>
   </div>
 </template>
