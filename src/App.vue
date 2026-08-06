397a21220ff82564993341bd06deb986af553771
dingzhen-vape <327112257@qq.com>
2026-08-06 19:23:39 +0800

    feat(player): 悬浮音乐播放器反色适配暗色主题

--- src/App.vue
+++ src/App.vue
@@ -74,7 +74,7 @@ async function getDailyWallpaper() {
   <!-- ============ 页面出口：当前路由组件渲染位置 ============ -->
   <main>
     <RouterView />
-    <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=22699105&auto=1&height=66"></iframe>
+    <iframe class="music-player" frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=22699105&auto=1&height=66"></iframe>
   </main>
 
 
